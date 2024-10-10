7744bb27d98973515cdee4d55db5859debc9a933
nahidhk <nahidappltd@gmail.com>
2024-10-10 14:43:52 +0600

    new data

--- javascript/login.js
+++ javascript/login.js
@@ -44,3 +44,28 @@ $(document).ready(function () {
         });
     }
 });
+
+
+function singupmod() {
+    const newpass = document.getElementById('newpass').value;
+    const conpass = document.getElementById('conpass').value;
+    if (newpass == conpass) {
+        blockbtn();
+        document.getElementById('newpass').style.border = "2px solid green";
+        document.getElementById('conpass').style.border = "2px solid green";
+        document.getElementById('passck').style.display = "none";
+    } else {
+        nonebtn();
+        document.getElementById('newpass').style.border = "2px solid red";
+        document.getElementById('conpass').style.border = "2px solid red";
+        document.getElementById('passck').innerHTML = "Password Don't match";
+    }
+}
+
+function blockbtn() {
+    document.getElementById("singbtn").style.display = "block";
+}
+
+function nonebtn() {
+    document.getElementById("singbtn").style.display = "none";
+}
