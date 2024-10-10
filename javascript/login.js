d6c1fb548f2d27b507796e5e1e1947410d071942
nahidhk <nahidappltd@gmail.com>
2024-10-10 15:11:19 +0600

    new data

--- javascript/login.js
+++ javascript/login.js
@@ -10,43 +10,6 @@ function loginchick(){
 console.log("javascript check");
 
 
-$(document).ready(function () {
-    $("#username").on("blur", function () {
-        var username = $(this).val();
-        $.ajax({
-            url: 'data.php', // Same file
-            type: 'POST',
-            data: { check_username: true, username: username },
-            success: function (response) {
-                $("#username-check").html(response);
-            }
-        });
-    });
-
-    $("#email").on("blur", function () {
-        var email = $(this).val();
-        $.ajax({
-            url: 'data.php', // Same file
-            type: 'POST',
-            data: { check_email: true, email: email },
-            success: function (response) {
-                $("#email-check").html(response);
-            }
-        });
-    });
-
-    $("#phone").on("blur", function () {
-        var phone = $(this).val();
-        $.ajax({
-            url: 'data.php', // Same file
-            type: 'POST',
-            data: { check_phone: true, phone: phone },
-            success: function (response) {
-                $("#phone-check").html(response);
-            }
-        });
-    });
-});
 
 function singupmod() {
     const newpass = document.getElementById('newpass').value;
--- singup/data.php
+++ singup/data.php
@@ -1,65 +1,37 @@
-<?php
+
+<?php 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $servername = "localhost";
-    $db_username = "readyof1";
-    $db_password = "A*Password123123";
+    $username = "readyof1";
+    $password = "A*Password123123";
     $dbname = "readyof1_Aanyface";
 
-    // Create connection
-    $conn = new mysqli($servername, $db_username, $db_password, $dbname);
+    // ডাটাবেজ কানেকশন তৈরি
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
-    // Check connection
+    // কানেকশন চেক করা
     if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+        die("কানেকশন ব্যর্থ: " . $conn->connect_error);
     }
 
-    if (isset($_POST['check_username'])) {
-        // Checking username availability
-        $username = mysqli_real_escape_string($conn, $_POST['username']);
-        $sql = "SELECT * FROM verifay_user WHERE username='$username'";
-        $result = $conn->query($sql);
-        
-        if ($result->num_rows > 0) {
-            echo "<span style=\"color: red;\"><i class=\"fa-regular fa-circle-xmark fa-shake\"></i> This Username Not Available!</span>";
-        } else {
-            echo "<span style=\"color: green;\"><i class=\"fa-regular fa-circle-check\"></i>This Username is Available!</span>";
-        }
-    } elseif (isset($_POST['check_email'])) {
-        $email = mysqli_real_escape_string($conn, $_POST['email']);
-        $sql = "SELECT * FROM verifay_user WHERE useremail='$email'";
-        $result = $conn->query($sql);
-        
-        if ($result->num_rows > 0) {
-            echo "<span style=\"color: red;\"><i class=\"fa-regular fa-circle-xmark fa-shake\"></i> This Email Already registered!</span>";
-        } else {
-            echo "<span style=\"color: green;\"><i class=\"fa-regular fa-circle-check\"></i>This Email is Available!</span>";
-        }
-    } elseif (isset($_POST['check_phone'])) {
-        $phone = mysqli_real_escape_string($conn, $_POST['phone']);
-        $sql = "SELECT * FROM verifay_user WHERE userphone='$phone'";
-        $result = $conn->query($sql);
-        
-        if ($result->num_rows > 0) {
-            echo "<span style=\"color: red;\"><i class=\"fa-regular fa-circle-xmark fa-shake\"></i> This number Already registered!</span>";
-        } else {
-            echo "<span style=\"color: green;\"><i class=\"fa-regular fa-circle-check\"></i>This Number is Available!</span>";
-        }
-    } elseif (isset($_POST['signup'])) {
-        $username = mysqli_real_escape_string($conn, $_POST['username']);
-        $phone = mysqli_real_escape_string($conn, $_POST['phone']);
-        $email = mysqli_real_escape_string($conn, $_POST['email']);
-        $bathdate = mysqli_real_escape_string($conn, $_POST['bath']);
-        $password = mysqli_real_escape_string($conn, $_POST['password']);
-        $sql = "INSERT INTO verifay_user (username, useremail, userphone, bath, password) 
-                VALUES ('$username', '$email', '$phone', '$bathdate', '$password')";
+    // ফর্মের ডেটা নিয়ে SQL ইনজেকশন থেকে রক্ষা পাওয়া
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $phone = mysqli_real_escape_string($conn, $_POST['phone']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $bathdate = mysqli_real_escape_string($conn, $_POST['bath']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
 
-        if ($conn->query($sql) === TRUE) {
-            echo "<script>window.location.href='/'</script>";
-        } else {
-            echo "ত্রুটি: " . $sql . "<br>" . $conn->error;
-        }
+    // SQL কুয়েরি তৈরি
+    $sql = "INSERT INTO verifay_user (username, useremail, userphone, bath, password) 
+            VALUES ('$username', '$email', '$phone', '$bathdate', '$password')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "নতুন রেকর্ড সফলভাবে যোগ হয়েছে";
+    } else {
+        echo "ত্রুটি: " . $sql . "<br>" . $conn->error;
     }
 
+    // কানেকশন বন্ধ করা
     $conn->close();
 }
 ?>
