7227752a1d31418373ca5749f3150242e3d2cadc
nahidhk <nahidappltd@gmail.com>
2024-10-10 14:18:28 +0600

    new data

--- javascript/login.js
+++ javascript/login.js
@@ -9,22 +9,66 @@ function loginchick(){
   }
 console.log("javascript check");
 
-
 document.addEventListener("DOMContentLoaded", function () {
+    // Check username availability
     document.getElementById("username").addEventListener("blur", function () {
         var username = this.value;
-        checkAvailability(username);
+        checkAvailability("username", username);
     });
 
-    function checkAvailability(username) {
+    // Check email availability
+    document.getElementById("email").addEventListener("blur", function () {
+        var email = this.value;
+        checkAvailability("email", email);
+    });
+
+    // Check phone availability
+    document.getElementById("phone").addEventListener("blur", function () {
+        var phone = this.value;
+        checkAvailability("phone", phone);
+    });
+
+    function checkAvailability(type, value) {
         var xhr = new XMLHttpRequest();
         xhr.open("POST", "data.php", true);
         xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
         xhr.onreadystatechange = function () {
             if (xhr.readyState === 4 && xhr.status === 200) {
-                console.log(xhr.responseText); // For debugging
+                if (type === "username") {
+                    document.getElementById('username-check').innerHTML = xhr.responseText;
+                } else if (type === "email") {
+                    document.getElementById('email-check').innerHTML = xhr.responseText;
+                } else if (type === "phone") {
+                    document.getElementById('phone-check').innerHTML = xhr.responseText;
+                }
             }
         };
-        xhr.send("check_username=true&username=" + encodeURIComponent(username));
+        xhr.send(type + '=true&' + type + '=' + encodeURIComponent(value));
     }
 });
+
+
+
+function singupmod() {
+    const newpass = document.getElementById('newpass').value;
+    const conpass = document.getElementById('conpass').value;
+    if (newpass == conpass) {
+        blockbtn();
+        document.getElementById('newpass').style.border = "2px solid green";
+        document.getElementById('conpass').style.border = "2px solid green";
+        document.getElementById('passck').style.display = "none";
+    } else {
+        nonebtn();
+        document.getElementById('newpass').style.border = "2px solid red";
+        document.getElementById('conpass').style.border = "2px solid red";
+        document.getElementById('passck').innerHTML = "Password Don't match";
+    }
+}
+
+function blockbtn() {
+    document.getElementById("singbtn").style.display = "block";
+}
+
+function nonebtn() {
+    document.getElementById("singbtn").style.display = "none";
+}
